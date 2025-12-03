04e012677bd37570bf15995ab51731c85b08f9ea
Raj singh <rajsingh5680x@gmail.com>
2025-12-03 23:31:16 +0530

    4

--- object/node.js
+++ object/node.js
@@ -1,22 +1,31 @@
-const obj = {
-    name:"raj",
-    account_balance:5000,
-    age:30 ,
-    gender:"male"
-}
-console.log(obj["gender"]);
+// const obj = {
+//     name:"raj",
+//     account_balance:5000,
+//     age:30 ,
+//     gender:"male"
+// }
+// let p= new Object();
+// p.name="ravi";
+// p.age=25;
+// p.account_balance=6000;
+// p.gender="male";
+// console.log(obj);
+// console.log(p); 
+
 
 //way to generate 
-class Person {
-    constructor(na, ag, sal){
-        this.name = na;
-        this.age = ag;
-        this.salary = sal;
+//code reusability 
+// class Person {
+//     constructor(na, ag, sal){
+//         this.name = na; //this.name is key and na is value
+//         this.age = ag;
+//         this.salary = sal;
+//     }
+// }
+// const person1= new Person("raj" ,23, 5000);
+// const person2= new Person("ravi" ,25, 6000);
+// console.log(person1);
+// console.log(person2);
+huiii bro 
 
-    }
 
-}
-const person1= new Person("raj" ,23, 5000);
-const person2= new Person("ravi" ,25, 6000);
-console.log(person1);
-console.log(person2);
