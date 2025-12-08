cdebf17229e5fac6b71677182d477603076e83f9
Raj singh <rajsingh5680x@gmail.com>
2025-12-08 18:36:13 +0530

    8th

---
+++ hoisting/index.js
@@ -0,0 +1,26 @@
+meet();
+function meet() {
+  console.log("Let's meet!");
+};
+//memmory allocaion   
+//  meet = function() {....}
+//execution phase
+// meet()  //Let's meet!
+
+// greet();
+// let greet= function(){
+//     console.log("Let's meet!");
+// }
+//memory allocation phase me greet ko memory milti h empty value k sath
+//execution phase me greet ko call krne pr error aayega
+//ReferenceError: Cannot access 'greet' before initialization
+
+
+
+
+// let greet= function(){
+//     console.log("Let's meet!");
+// }
+// greet();
+//memory allocation phase me greet ko memory milti h empty value k sath
+// execution phase me greet me function assign hone k bad greet ko call krne pr "Let's meet!" print hoga
